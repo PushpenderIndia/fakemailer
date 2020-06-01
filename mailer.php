c40d90ce43fd54f8ed759fda8c0bddb7c352f7cb
Pushpender <56433698+Technowlogy-Pushpender@users.noreply.github.com>
2020-06-01 11:22:26 +0530

    Add files via upload

---
+++ mailer.php
@@ -0,0 +1,52 @@
+<?php
+
+$to = $_POST['r_email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];  
+
+$sender_name = $_POST['s_name'];
+$sender_email = $_POST['s_email'];
+
+$random_hash = md5(date('r', time())); 
+
+$headers = "From: " . $sender_name . "<" . $sender_email . ">";
+$headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\""; 
+
+$mail_sent = mail( $to, $subject, $message, $headers );
+
+if($mail_sent)
+	{
+	echo "
+	
+<html>
+<head>
+	<title>Success : )</title>
+</head>
+	
+<body>
+<script>
+	alert(\"Email Sended Successfully!\");
+	window.location.replace(\"index.html\");
+</script>
+</body>
+</html>  ";
+	}
+	
+else {
+	echo "
+	
+<html>
+<head>
+	<title>Failed : (</title>
+</head>
+	
+<body>
+<script>
+	alert(\"Failed to Send Email !\");
+	window.location.replace(\"index.html\");
+</script>
+</body>
+</html>  ";
+}
+
+?>
